024782087549a97cb0cf1ec567e27bd455885b3a
Neoprot <kaua.seichi.gomes@gmail.com>
2024-08-03 18:38:52 -0300

    remaking structure of the project, create components file, adding some images for delete and visualizing attachments and products, creating some alerts about the empty fields and making the code show the json in console

--- js/main.js
+++ js/main.js
@@ -1,74 +1,88 @@
 $(document).ready(function () {
-    let anexoFiles = [];
+    if (window.eventsBound) return;
+    window.eventsBound = true;
 
-    // Add new product
-    $('#addProduto').click(function () {
+    let anexoFiles = [];
+    let produtoCounter = 0; // Contador de produtos
+
+    // Adicionar novo produto
+    $(document).on('click', '#addProduto', function () {
+        produtoCounter++; // Incrementa o contador de produtos
         let productTemplate = `
-        <div class="produto-item fs-lg-margin-bottom">
-            <div class="form-row">
-                <div class="form-group col-md-6">
-                    <label>Produto</label>
-                    <input type="text" class="form-control produtoDescricao" required>
+        <div class="fs-display-flex fs-align-items-center produto-item">
+            <div class="form-group fs-display-flex fs-margin-left-auto col-md-1">
+                <button type="button" class="btn btn-danger btn-remove-produto"><img src="style-guide/images/illustrations/delete.svg" class="img-remove-produto illustration illustration-xs" alt="Excluir Produto"></button>
+            </div>
+            <div class="panel panel-default col-md-11">
+                <div class="panel-heading fs-no-bg">
+                    <h3 class="panel-title">Produto - <span class="produto-indice">${produtoCounter}</span></h3>
                 </div>
-                <div class="form-group col-md-2">
-                    <label>UND. Medida</label>
-                    <select class="form-control produtoUnidade" required>
-                        <option value="">Selecione</option>
-                        <option value="kg">Kg</option>
-                        <option value="un">Unidade</option>
-                    </select>
-                </div>
-                <div class="form-group col-md-2">
-                    <label>QTD. em Estoque</label>
-                    <input type="number" class="form-control produtoQtde" required>
-                </div>
-                <div class="form-group col-md-2">
-                    <label>Valor Unitário</label>
-                    <input type="number" class="form-control produtoValor" step="0.01" required>
-                </div>
-                <div class="form-group col-md-2">
-                    <label>Valor Total</label>
-                    <input type="number" class="form-control produtoTotal" readonly>
-                </div>
-                <div class="form-group col-md-offset-6 col-md-1 fs-display-flex fs-align-items-flex-end">
-                    <button type="button" class="btn btn-danger btn-remove-produto">Excluir</button>
+                <div class="form-row panel-body">
+                    <div class="form-group col-md-2">
+                        <img src="/style-guide/images/illustrations/package-diagram.svg" class="illustration illustration-package-diagram illustration-xl" title="package-diagram">
+                    </div>
+                    <div class="form-group col-md-10">
+                        <label>Produto</label>
+                        <input type="text" class="form-control produtoDescricao" required>
+                    </div>
+                    <div class="form-group col-md-2">
+                        <label>UND. Medida</label>
+                        <select class="form-control produtoUnidade" required>
+                            <option value="">Selecione</option>
+                            <option value="kg">Kg</option>
+                            <option value="un">Unidade</option>
+                        </select>
+                    </div>
+                    <div class="form-group col-md-3">
+                        <label>QTD. em Estoque</label>
+                        <input type="number" class="form-control produtoQtde" required>
+                    </div>
+                    <div class="form-group col-md-3">
+                        <label>Valor Unitário</label>
+                        <input type="number" class="form-control produtoValor" step="0.01" required>
+                    </div>
+                    <div class="form-group col-md-2">
+                        <label>Valor Total</label>
+                        <input type="number" class="form-control produtoTotal" readonly>
+                    </div>
                 </div>
             </div>
         </div>`;
         $('#produtosContainer').append(productTemplate);
     });
 
-    // Remove product
+    // Remover produto
     $(document).on('click', '.btn-remove-produto', function () {
         $(this).closest('.produto-item').remove();
+        produtoCounter--;
     });
 
-    // Add new attachment
-    $('#addAnexo').click(function () {
+    // Adicionar novo anexo
+    $(document).on('click', '#addAnexo', function () {
         let anexoTemplate = `
-        <div class="anexo-item fs-lg-margin-bottom">
+        <div class="anexo-item">
+            <div class="form-group col-md-1 d-flex fs-lg-margin-top">
+                <button type="button" class="btn btn-danger btn-remove-anexo"><img src="style-guide/images/illustrations/delete.svg" class="illustration illustration-xs" alt="Excluir Anexo"></button>
+            </div>
+            <div class="form-group col-md-1 d-flex fs-lg-margin-top">
+                <button type="button" class="btn btn-info btn-view-anexo"><img src="style-guide/images/illustrations/visualize.svg" class="illustration illustration-xs" alt="Vizualizar Anexo"></button>
+            </div>
             <div class="form-row">
                 <div class="form-group col-md-10">
                     <label>Documento anexo</label>
-                    <input type="file" class="form-control anexoDocumento" required>
-                </div>
-                <div class="form-group col-md-1 d-flex align-items-end">
-                    <button type="button" class="btn btn-danger btn-remove-anexo">Excluir</button>
-                </div>
-                <div class="form-group col-md-1 d-flex align-items-end">
-                    <button type="button" class="btn btn-primary btn-view-anexo">Visualizar</button>
+                    <input type="file" class="form-control anexoDocumento input-lg" required>
                 </div>
             </div>
         </div>`;
         $('#anexosContainer').append(anexoTemplate);
     });
 
-    // Remove attachment
+    // Remover anexo
     $(document).on('click', '.btn-remove-anexo', function () {
         $(this).closest('.anexo-item').remove();
     });
 
-    // View attachment
+    // Visualizar anexo
     $(document).on('click', '.btn-view-anexo', function () {
         let fileInput = $(this).closest('.anexo-item').find('.anexoDocumento')[0];
         if (fileInput.files.length > 0) {
@@ -83,7 +97,7 @@ $(document).ready(function () {
         }
     });
 
-    // Calculate total value
+    // Calcular valor total
     $(document).on('input', '.produtoQtde, .produtoValor', function () {
         let item = $(this).closest('.produto-item');
         let qtde = item.find('.produtoQtde').val();
@@ -92,12 +106,49 @@ $(document).ready(function () {
         item.find('.produtoTotal').val(total.toFixed(2));
     });
 
-    // Form submission
-    $('#formFornecedor').submit(function (event) {
+    // Evento para o botão Salvar Fornecedor
+    $(document).on('click', '#salvarFornecedor', function (event) {
         event.preventDefault();
-        $('#loadingModal').modal('show');
+        let isValid = true;
 
-        // Gather form data
+        // Verifica todos os campos required no formulário
+        $('#formFornecedor [required]').each(function () {
+            let formGroup = $(this).closest('.form-group');
+            if ($(this).val() === '') {
+                isValid = false;
+                formGroup.addClass('has-error'); // Adiciona classe de erro
+            } else {
+                formGroup.removeClass('has-error'); // Remove classe de erro se preenchido
+            }
+        });
+
+        // Verifica todos os campos required nos produtos
+        $('#produtosContainer .produto-item [required]').each(function () {
+            let formGroup = $(this).closest('.form-group');
+            if ($(this).val() === '') {
+                isValid = false;
+                formGroup.addClass('has-error'); // Adiciona classe de erro
+            } else {
+                formGroup.removeClass('has-error'); // Remove classe de erro se preenchido
+            }
+        });
+
+        // Verifica todos os campos required nos anexos
+        $('#anexosContainer .anexo-item [required]').each(function () {
+            let formGroup = $(this).closest('.form-group');
+            if ($(this).val() === '') {
+                isValid = false;
+                formGroup.addClass('has-error'); // Adiciona classe de erro
+            } else {
+                formGroup.removeClass('has-error'); // Remove classe de erro se preenchido
+            }
+        });
+
+        if (!isValid) {
+            alert("Por favor, preencha todos os campos obrigatórios."); // Exibe o alerta de erro
+            return;
+        }
+        // Coletar dados do formulário
         let formData = {
             razaoSocial: $('#razaoSocial').val(),
             nomeFantasia: $('#nomeFantasia').val(),
@@ -117,7 +168,7 @@ $(document).ready(function () {
             anexos: []
         };
 
-        // Gather products data
+        // Coletar dados dos produtos
         $('#produtosContainer .produto-item').each(function (index, element) {
             let produto = {
                 indice: index + 1,
@@ -130,7 +181,7 @@ $(document).ready(function () {
             formData.produtos.push(produto);
         });
 
-        // Gather attachments data
+        // Coletar dados dos anexos
         let filesProcessed = 0;
         let totalFiles = $('#anexosContainer .anexo-item').length;
         $('#anexosContainer .anexo-item').each(function (index, element) {
@@ -142,34 +193,35 @@ $(document).ready(function () {
                     let anexo = {
                         indice: index + 1,
                         nomeArquivo: file.name,
-                        blobArquivo: e.target.result
+                        conteudoArquivo: e.target.result
                     };
                     formData.anexos.push(anexo);
-
-                    // Check if all files are processed
                     filesProcessed++;
                     if (filesProcessed === totalFiles) {
-                        // Output the result
-                        console.log(JSON.stringify(formData, null, 2));
-                        $('#loadingModal').modal('hide');
+                        // Submeter os dados do formulário
+                        enviarDadosFormulario(formData);
                     }
                 };
                 reader.readAsDataURL(file);
             } else {
-                // If no files, still need to increment the counter
                 filesProcessed++;
-                if (filesProcessed === totalFiles) {
-                    // Output the result
-                    console.log(JSON.stringify(formData, null, 2));
-                    $('#loadingModal').modal('hide');
-                }
             }
         });
-        console.log(JSON.stringify(formData, null, 2));
+
+        if (totalFiles === 0) {
+            enviarDadosFormulario(formData);
+        }
     });
 
-    // Fetch address by CEP
-    $('#cep').blur(function () {
+    // Função para enviar dados do formulário
+    function enviarDadosFormulario(formData) {
+        console.log(formData);
+        // Aqui você pode enviar os dados para o backend ou realizar outra ação desejada
+
+        // exibir mensagem de sucesso
+        alert("Dados enviados com sucesso!");
+    }
+    $(document).on('blur', '#cep', function () {
         let cep = $(this).val().replace(/\D/g, '');
         if (cep != "") {
             $.getJSON(`https://viacep.com.br/ws/${cep}/json/`, function (data) {
